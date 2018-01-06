85ea652212bf2a8c76dee4f19814389ea2b5ad5c
Khang Nguyen <khang.neon.1997@gmail.com>
2018-01-06 10:16:18 +0700

    Add LeakCanary to detect mem leak

--- app/src/main/java/com/github/khangnt/mcp/MainApplication.kt
+++ app/src/main/java/com/github/khangnt/mcp/MainApplication.kt
@@ -1,6 +1,7 @@
 package com.github.khangnt.mcp
 
 import android.app.Application
+import com.squareup.leakcanary.LeakCanary
 import timber.log.Timber
 
 /**
@@ -12,6 +13,12 @@ class MainApplication: Application() {
 
     override fun onCreate() {
         super.onCreate()
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            // This process is dedicated to LeakCanary for heap analysis.
+            // You should not init your app in this process.
+            return;
+        }
+        LeakCanary.install(this)
 
         SingletonInstances.init(this)
 
