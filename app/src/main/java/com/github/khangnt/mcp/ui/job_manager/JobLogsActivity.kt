1eb5293cdae6bbbb95a576df982a56a97dc3d666
Simon Pham <simonpham.dn@gmail.com>
2018-01-28 17:05:54 +0700

    Add reload & switch job functions

--- app/src/main/java/com/github/khangnt/mcp/ui/job_manager/JobLogsActivity.kt
+++ app/src/main/java/com/github/khangnt/mcp/ui/job_manager/JobLogsActivity.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.ScrollView
 import com.github.khangnt.mcp.R
 import com.github.khangnt.mcp.worker.WorkingPaths
 import com.github.khangnt.mcp.worker.makeWorkingPaths
@@ -37,18 +38,33 @@ class JobLogsActivity : AppCompatActivity() {
         jobId = intent.getLongExtra(EXTRA_JOB_ID, -1)
         check(jobId > -1, { "Invalid job ID: $jobId" })
 
-        val workingPaths: WorkingPaths = makeWorkingPaths(applicationContext)
-        logFile = workingPaths.getLogFileOfJob(jobId)
-
-        disposable = Observable.interval(0, 5, TimeUnit.SECONDS)
-                .map { logFile!!.readText() }
-                .distinctUntilChanged()
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe({ text -> tvLogs.text = text }, Timber::e)
+        switchJob(jobId)
     }
 
     override fun onDestroy() {
         super.onDestroy()
         disposable?.dispose()
+    }
+
+    fun reload() {
+        disposable?.dispose()
+        disposable = Observable.interval(0, 5, TimeUnit.SECONDS)
+                .map { logFile!!.readText() }
+                .distinctUntilChanged()
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe({ text -> tvLogs.text = text }, Timber::e)
+
+        scrollToBottom()
+    }
+
+    fun switchJob(jobId: Long) {
+        val workingPaths: WorkingPaths = makeWorkingPaths(applicationContext)
+        logFile = workingPaths.getLogFileOfJob(jobId)
+
+        reload()
+    }
+
+    fun scrollToBottom() {
+        scrollLogs.postDelayed(Runnable { scrollLogs.fullScroll(ScrollView.FOCUS_DOWN) }, 100)
     }
 }
