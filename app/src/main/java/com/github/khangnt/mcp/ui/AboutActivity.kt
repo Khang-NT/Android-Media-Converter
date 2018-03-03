94be204108a39734e424013f446e20e0aeb97bfe
Simon Pham <simonpham.dn@gmail.com>
2018-03-03 19:02:39 +0700

    Show/hide "Feedback by email" card

--- app/src/main/java/com/github/khangnt/mcp/ui/AboutActivity.kt
+++ app/src/main/java/com/github/khangnt/mcp/ui/AboutActivity.kt
@@ -1,10 +1,14 @@
 package com.github.khangnt.mcp.ui
 
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
 import android.content.Context
 import android.content.Intent
 import android.graphics.Color
 import android.os.Bundle
 import android.support.v4.graphics.drawable.DrawableCompat
+import android.view.View
+import android.widget.ScrollView
 import com.github.khangnt.mcp.BuildConfig
 import com.github.khangnt.mcp.GITHUB_REPO
 import com.github.khangnt.mcp.PLAY_STORE_PACKAGE
@@ -65,6 +69,40 @@ class AboutActivity : BaseActivity() {
                     .setNotices(R.raw.licenses)
                     .build()
                     .show()
+        }
+
+        val cardHeight = cardFeedback.height.toFloat()
+        emailFeedback.setOnClickListener {
+            if (cardFeedback.visibility == View.GONE) {
+                cardFeedback.visibility = View.VISIBLE
+                cardFeedback.alpha = 0.0f
+
+                cardFeedback.animate()
+                        .translationY(cardHeight)
+                        .alpha(1.0f)
+                        .setListener(object : AnimatorListenerAdapter() {
+                            override fun onAnimationEnd(animation: Animator) {
+                                super.onAnimationEnd(animation)
+                                edFeedbackDetails.requestFocus()
+                            }
+
+                            override fun onAnimationStart(animation: Animator?) {
+                                super.onAnimationStart(animation)
+                                scrollView.post({ scrollView.fullScroll(ScrollView.FOCUS_DOWN) })
+                            }
+                        })
+            } else {
+                cardFeedback.animate()
+                        .translationY(0.0f)
+                        .alpha(0.0f)
+                        .setListener(object : AnimatorListenerAdapter() {
+                            override fun onAnimationEnd(animation: Animator) {
+                                super.onAnimationEnd(animation)
+                                cardFeedback.visibility = View.GONE
+                            }
+                        })
+            }
+
         }
 
     }
