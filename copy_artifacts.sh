277ffc39f5fe812e6b32af96f873a79e032d54c0
Khang Nguyen <khang.neon.1997@gmail.com>
2018-03-08 16:59:21 +0700

    Update CircleCI config: fix missing report file when build failed

--- copy_artifacts.sh
+++ copy_artifacts.sh
@@ -3,22 +3,24 @@
 set -e
 
 currentDir=$(pwd)
-artifact_folder="$currentDir/artifacts"; mkdir ${artifact_folder}
-apk_folder="$artifact_folder/apks"; mkdir ${apk_folder}
-mapping_folder="$artifact_folder/mappings"; mkdir ${mapping_folder}
-report_folder="$artifact_folder/reports"; mkdir ${report_folder}
+artifact_folder="$currentDir/artifacts"; mkdir -p ${artifact_folder}
+apk_folder="$artifact_folder/apks"; mkdir -p ${apk_folder}
+mapping_folder="$artifact_folder/mappings"; mkdir -p ${mapping_folder}
+report_folder="$artifact_folder/reports"; mkdir -p ${report_folder}
 
 for path in $(find app/build/outputs/apk -name '*.apk');
 do
-    cp ${path} ${apk_folder}
+  cp ${path} ${apk_folder}
 done
 
-pushd app/build/outputs/mapping
-for path in $(find . -name 'mapping.txt');
-do
-    mkdir -p $(dirname "$mapping_folder/$path")
-    cp ${path} "$mapping_folder/$path"
-done
-popd
+if [ -d app/build/outputs/mapping ]; then
+  pushd app/build/outputs/mapping
+    for path in $(find . -name 'mapping.txt');
+    do
+      mkdir -p $(dirname "$mapping_folder/$path")
+      cp ${path} "$mapping_folder/$path"
+    done
+  popd
+fi
 
 cp -r app/build/reports/* ${report_folder}
