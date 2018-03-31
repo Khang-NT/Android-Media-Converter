3a7e4f17c297c5b0e5c097756efbe9cd772e1d34
Simon Pham <simonpham.dn@gmail.com>
2018-03-31 23:37:27 +0700

    Add trim options to Convert Aac Fragment

--- app/src/main/java/com/github/khangnt/mcp/ui/presetcmd/aac/ConvertAacFragment.kt
+++ app/src/main/java/com/github/khangnt/mcp/ui/presetcmd/aac/ConvertAacFragment.kt
@@ -2,12 +2,18 @@ package com.github.khangnt.mcp.ui.presetcmd.aac
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.support.transition.Fade
+import android.support.transition.Slide
+import android.support.transition.TransitionManager
+import android.support.transition.TransitionSet
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.github.khangnt.mcp.R
 import com.github.khangnt.mcp.ui.presetcmd.ConvertFragment
 import com.github.khangnt.mcp.ui.presetcmd.common.SingleInputOutputFragment
+import com.github.khangnt.mcp.ui.presetcmd.common.TrimmerFragment
 import com.github.khangnt.mcp.util.onSeekBarChanged
 import com.github.khangnt.mcp.worker.ConverterService
 import kotlinx.android.synthetic.main.fragment_convert_aac.*
@@ -38,12 +44,29 @@ class ConvertAacFragment : ConvertFragment() {
         sbQuality.max = CBR_MAX - CBR_MIN
         sbQuality.progress = CBR_RECOMMEND - CBR_MIN
         sbQuality.onSeekBarChanged { updateQualityText() }
+
+        btnAdvancedToggle.setOnClickListener { toggleAdvanced() }
         btnStartConversion.setOnClickListener { validateAndStartConversion() }
     }
 
     override fun onViewStateRestored(savedInstanceState: Bundle?) {
         super.onViewStateRestored(savedInstanceState)
         updateQualityText()
+    }
+
+    private fun toggleAdvanced() {
+        val transition = TransitionSet()
+        transition.addTransition(Fade())
+        transition.addTransition(Slide(Gravity.TOP))
+        TransitionManager.beginDelayedTransition(advancedLayout, transition)
+
+        if (advancedLayout.visibility == View.GONE) {
+            advancedLayout.visibility = View.VISIBLE
+            btnAdvancedToggle.text = getString(R.string.advanced_arrow_up)
+        } else {
+            advancedLayout.visibility = View.GONE
+            btnAdvancedToggle.text = getString(R.string.advanced_arrow_down)
+        }
     }
 
     @SuppressLint("SetTextI18n")
@@ -56,23 +79,36 @@ class ConvertAacFragment : ConvertFragment() {
         return fragment as SingleInputOutputFragment
     }
 
+    private fun getTrimFragment(): TrimmerFragment {
+        val fragment = childFragmentManager.findFragmentById(R.id.fragmentTrimmer)
+        return fragment as TrimmerFragment
+    }
+
     private fun validateAndStartConversion() {
         getIoFragment().validateAndGetInputOutputData { inputOutputData ->
             val cmdArgsBuilder = StringBuffer()
             cmdArgsBuilder.append("-hide_banner -map 0:a -map_metadata 0:g -codec:a aac ")
                     .append("-b:a ${CBR_MIN + sbQuality.progress}k ")
 
-            ConverterService.newJob(
-                    context!!,
-                    title = inputOutputData.title,
-                    inputs = listOf(inputOutputData.inputUri),
-                    args = cmdArgsBuilder.toString(),
-                    outputUri = inputOutputData.outputUri,
-                    outputFormat = "ipod"
-            )
+            getTrimFragment().validateAndGetBeginEndPostition { beginEndPosition ->
+                if (beginEndPosition.isTrimmed) {
+                    cmdArgsBuilder.append("-ss ${beginEndPosition.beginPos} -t ${beginEndPosition.endPos} ")
+                }
 
-            (activity as? OnSubmittedListener)?.onSubmitted(this)
-                    ?: Timber.w("Host activity does not implement OnSubmittedListener")
+                if (!beginEndPosition.isError) {
+                    ConverterService.newJob(
+                            context!!,
+                            title = inputOutputData.title,
+                            inputs = listOf(inputOutputData.inputUri),
+                            args = cmdArgsBuilder.toString(),
+                            outputUri = inputOutputData.outputUri,
+                            outputFormat = "ipod"
+                    )
+
+                    (activity as? OnSubmittedListener)?.onSubmitted(this)
+                            ?: Timber.w("Host activity does not implement OnSubmittedListener")
+                }
+            }
         }
     }
 
