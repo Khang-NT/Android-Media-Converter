d58cd7e0adabb70827107b3a12a51f979a8ff911
Simon Pham <simonpham.dn@gmail.com>
2018-03-31 23:59:46 +0700

    Add trim options to Convert Mp4 Fragment

--- app/src/main/java/com/github/khangnt/mcp/ui/presetcmd/mp4/ConvertMp4Fragment.kt
+++ app/src/main/java/com/github/khangnt/mcp/ui/presetcmd/mp4/ConvertMp4Fragment.kt
@@ -1,12 +1,18 @@
 package com.github.khangnt.mcp.ui.presetcmd.mp4
 
 import android.os.Bundle
+import android.support.transition.Fade
+import android.support.transition.Slide
+import android.support.transition.TransitionManager
+import android.support.transition.TransitionSet
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.github.khangnt.mcp.R
 import com.github.khangnt.mcp.ui.presetcmd.ConvertFragment
 import com.github.khangnt.mcp.ui.presetcmd.common.SingleInputOutputFragment
+import com.github.khangnt.mcp.ui.presetcmd.common.TrimmerFragment
 import com.github.khangnt.mcp.worker.ConverterService
 import kotlinx.android.synthetic.main.fragment_convert_mp4.*
 import timber.log.Timber
@@ -36,6 +42,7 @@ class ConvertMp4Fragment : ConvertFragment() {
         getIoFragment().autoFillExt = "mp4"
         spinnerVideoQuality.setSelection(1)
 
+        btnAdvancedToggle.setOnClickListener { toggleAdvanced() }
         btnStartConversion.setOnClickListener { validateAndStartConversion() }
     }
 
@@ -43,9 +50,29 @@ class ConvertMp4Fragment : ConvertFragment() {
         super.onViewStateRestored(savedInstanceState)
     }
 
+    private fun toggleAdvanced() {
+        val transition = TransitionSet()
+        transition.addTransition(Fade())
+        transition.addTransition(Slide(Gravity.TOP))
+        TransitionManager.beginDelayedTransition(advancedLayout, transition)
+
+        if (advancedLayout.visibility == View.GONE) {
+            advancedLayout.visibility = View.VISIBLE
+            btnAdvancedToggle.text = getString(R.string.advanced_arrow_up)
+        } else {
+            advancedLayout.visibility = View.GONE
+            btnAdvancedToggle.text = getString(R.string.advanced_arrow_down)
+        }
+    }
+
     private fun getIoFragment(): SingleInputOutputFragment {
         val fragment = childFragmentManager.findFragmentById(R.id.fragmentInputOutput)
         return fragment as SingleInputOutputFragment
+    }
+
+    private fun getTrimFragment(): TrimmerFragment {
+        val fragment = childFragmentManager.findFragmentById(R.id.fragmentTrimmer)
+        return fragment as TrimmerFragment
     }
 
     private fun validateAndStartConversion() {
@@ -54,17 +81,26 @@ class ConvertMp4Fragment : ConvertFragment() {
 
             cmdArgsBuilder.append("-q:v ${mp4VideoQuality[spinnerVideoQuality.selectedItemPosition]} ")
 
-            ConverterService.newJob(
-                    context!!,
-                    title = inputOutputData.title,
-                    inputs = listOf(inputOutputData.inputUri),
-                    args = cmdArgsBuilder.toString(),
-                    outputUri = inputOutputData.outputUri,
-                    outputFormat = "mp4"
-            )
 
-            (activity as? OnSubmittedListener)?.onSubmitted(this)
-                    ?: Timber.w("Host activity does not implement OnSubmittedListener")
+            getTrimFragment().validateAndGetBeginEndPostition { beginEndPosition ->
+                if (beginEndPosition.isTrimmed) {
+                    cmdArgsBuilder.append("-ss ${beginEndPosition.beginPos} -t ${beginEndPosition.endPos} ")
+                }
+
+                if (!beginEndPosition.isError) {
+                    ConverterService.newJob(
+                            context!!,
+                            title = inputOutputData.title,
+                            inputs = listOf(inputOutputData.inputUri),
+                            args = cmdArgsBuilder.toString(),
+                            outputUri = inputOutputData.outputUri,
+                            outputFormat = "mp4"
+                    )
+
+                    (activity as? OnSubmittedListener)?.onSubmitted(this)
+                            ?: Timber.w("Host activity does not implement OnSubmittedListener")
+                }
+            }
         }
     }
 
