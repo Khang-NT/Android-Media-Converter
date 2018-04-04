035d03a175d80317dfda92f1b0a8de15e78d3b00
Khang Nguyen <khang.neon.1997@gmail.com>
2018-04-05 00:53:28 +0700

    Refactor JobManagerFragment with android architecture components

--- app/src/main/java/com/github/khangnt/mcp/ui/jobmanager/JobManagerFragment.kt
+++ app/src/main/java/com/github/khangnt/mcp/ui/jobmanager/JobManagerFragment.kt
@@ -1,6 +1,6 @@
 package com.github.khangnt.mcp.ui.jobmanager
 
-import android.content.Intent
+import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
 import android.support.v7.widget.GridLayoutManager
 import android.view.LayoutInflater
@@ -8,24 +8,13 @@ import android.view.View
 import android.view.ViewGroup
 import com.github.khangnt.mcp.R
 import com.github.khangnt.mcp.SingletonInstances
-import com.github.khangnt.mcp.annotation.JobStatus.*
-import com.github.khangnt.mcp.job.jobComparator
-import com.github.khangnt.mcp.reportNonFatal
 import com.github.khangnt.mcp.ui.BaseFragment
-import com.github.khangnt.mcp.ui.common.AdapterModel
 import com.github.khangnt.mcp.ui.common.HeaderModel
-import com.github.khangnt.mcp.ui.common.ItemHeaderViewHolder
 import com.github.khangnt.mcp.ui.common.MixAdapter
+import com.github.khangnt.mcp.ui.common.Status
 import com.github.khangnt.mcp.ui.decorator.ItemOffsetDecoration
 import com.github.khangnt.mcp.util.getSpanCount
-import com.github.khangnt.mcp.view.RecyclerViewGroupState
-import com.github.khangnt.mcp.worker.ConverterService
-import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.fragment_job_manager.*
-import timber.log.Timber
-import java.util.*
-import java.util.concurrent.TimeUnit
 
 /**
  * Created by Khang NT on 1/5/18.
@@ -33,27 +22,24 @@ import java.util.concurrent.TimeUnit
  */
 
 class JobManagerFragment : BaseFragment() {
-    private val jobManager = SingletonInstances.getJobManager()
-    private val runningHeaderModel = RunningHeaderModel("Running",
-            jobManager.getLiveLogObservable(), { disposeOnPaused() })
-    private val preparingHeaderModel = HeaderModel("Preparing")
-    private val readyHeaderModel = HeaderModel("Ready")
-    private val pendingHeaderModel = HeaderModel("Pending")
-    private val finishedHeaderModel = HeaderModel("Finished")
 
+    private lateinit var viewModel: JobManagerViewModel
     private lateinit var adapter: MixAdapter
-    private lateinit var recyclerViewGroupState: RecyclerViewGroupState
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        retainInstance = true
-        context!!.startService(Intent(context!!, ConverterService::class.java))
-        adapter = MixAdapter.Builder(idGeneratorScope = "JobManagerFragment")
-                .register(HeaderModel::class.java, ItemHeaderViewHolder.Factory)
-                .register(RunningHeaderModel::class.java, ItemHeaderRunningViewHolder.Factory)
-                .register(JobModel::class.java, ItemJobViewHolder.Factory)
-                .build()
-        recyclerViewGroupState = RecyclerViewGroupState().setRetryFunc(this::loadData)
+        SingletonInstances.getJobWorkerMangager().maybeLaunchWorker()
+
+        viewModel = ViewModelProviders.of(this, SingletonInstances.getViewModelFactory())
+                .get(JobManagerViewModel::class.java)
+        adapter = viewModel.createNewAdapter()
+
+        val latestStatus = viewModel.getStatus().value
+        val latestJobs = viewModel.getAdapterModel().value.orEmpty()
+        if (latestStatus is Status.Error // auto reload
+                || latestStatus != Status.Loading && latestJobs.isEmpty()) {
+            viewModel.reload()
+        }
     }
 
     override fun onCreateView(
@@ -65,78 +51,33 @@ class JobManagerFragment : BaseFragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setActivitySupportActionBar(toolbar)
-        ItemOffsetDecoration(context!!)
-                .setHorizontalSpace(R.dimen.margin_normal)
-                .setVerticalSpace(R.dimen.margin_small)
-                .applyTo(recyclerViewGroup.getRecyclerView())
-        val itemJobMinWidth = resources.getDimensionPixelOffset(R.dimen.item_job_min_width)
-        val columnSpace = resources.getDimensionPixelOffset(R.dimen.margin_normal)
-        val spanCount = getSpanCount(itemJobMinWidth, columnSpace)
-        val lm = GridLayoutManager(view.context, spanCount)
-        lm.spanSizeLookup = object: GridLayoutManager.SpanSizeLookup() {
-            override fun getSpanSize(position: Int): Int {
-                val data = try {
-                    adapter.getData(position)
-                } catch (error: IndexOutOfBoundsException) {
-                    // https://issuetracker.google.com/issues/37132919
-                    reportNonFatal(
-                            throwable = error,
-                            where = "getSpanSize",
-                            message = "Call by thread: ${Thread.currentThread().name}"
-                    )
-                    return 1
+        with(recyclerViewContainer) {
+            onRefreshListener = { viewModel.reload(); true }
+            ItemOffsetDecoration(context!!)
+                    .setHorizontalSpace(R.dimen.margin_normal)
+                    .setVerticalSpace(R.dimen.margin_small)
+                    .applyTo(getRecyclerView())
+            val itemJobMinWidth = resources.getDimensionPixelOffset(R.dimen.item_job_min_width)
+            val columnSpace = resources.getDimensionPixelOffset(R.dimen.margin_normal)
+            val spanCount = getSpanCount(itemJobMinWidth, columnSpace)
+            val lm = GridLayoutManager(view.context, spanCount)
+            lm.spanSizeLookup = object : GridLayoutManager.SpanSizeLookup() {
+                override fun getSpanSize(position: Int): Int {
+                    val data = adapter.getItemData(position)
+                    return if (data is HeaderModel || data is LiveHeaderModel) spanCount else 1
                 }
-                return if (data is HeaderModel) spanCount else 1
             }
+
+            getRecyclerView().layoutManager = lm
+            getRecyclerView().adapter = adapter
         }
-        recyclerViewGroupState.bind(recyclerViewGroup, adapter, lm)
-    }
-
-    override fun onResume() {
-        super.onResume()
-        loadData()
-    }
-
-    private fun loadData() {
-        recyclerViewGroupState.loading()
-        jobManager.getJob(RUNNING, PREPARING, READY, PENDING, COMPLETED, FAILED)
-                .throttleLast(400, TimeUnit.MILLISECONDS)
-                .observeOn(Schedulers.computation())
-                .doOnNext { Collections.sort(it, jobComparator) }
-                .map { originalList ->
-                    val listModels = mutableListOf<AdapterModel>()
-                    var addedFinishedHeader = false
-                    var previousStatus: Int? = null
-                    originalList.forEach { item ->
-                        if (item.status != previousStatus) {
-                            previousStatus = item.status
-                            if (item.status == RUNNING) {
-                                listModels.add(runningHeaderModel)
-                            } else if (item.status == PREPARING) {
-                                listModels.add(preparingHeaderModel)
-                            } else if (item.status == READY) {
-                                listModels.add(readyHeaderModel)
-                            } else if (item.status == PENDING) {
-                                listModels.add(pendingHeaderModel)
-                            } else if (!addedFinishedHeader) {
-                                addedFinishedHeader = true
-                                listModels.add(finishedHeaderModel)
-                            }
-                        }
-                        listModels.add(JobModel(item))
-                    }
-                    return@map listModels
-                }
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe({ list ->
-                    adapter.setData(list)
-                    recyclerViewGroupState.checkData(list)
-                }, { error ->
-                    Timber.e(error, "Load job list failed")
-                    adapter.setData(emptyList())
-                    recyclerViewGroupState.error(error.message)
-                })
-                .disposeOnPaused(tag = "loadData")
+        viewModel.getAdapterModel().observe { adapterModels ->
+            adapter.setData(adapterModels)
+            recyclerViewContainer.setShowEmptyState(adapterModels.isEmpty())
+        }
+        viewModel.getStatus().observe { status ->
+            recyclerViewContainer.setStatus(status)
+        }
     }
 
 }
