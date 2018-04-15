07ad3832093439d82ceafa4273e0afd8cfaab30a
Simon Pham <simonpham.dn@gmail.com>
2018-04-15 13:06:03 +0700

    Fragment selected files

--- app/src/main/java/com/github/khangnt/mcp/ui/jobmaker/SelectedFilesFragment.kt
+++ app/src/main/java/com/github/khangnt/mcp/ui/jobmaker/SelectedFilesFragment.kt
@@ -23,7 +23,7 @@ class SelectedFilesFragment : StepFragment() {
             inflater: LayoutInflater,
             container: ViewGroup?,
             savedInstanceState: Bundle?
-    ): View? = inflater.inflate(R.layout.fragment_todo, container, false)
+    ): View? = inflater.inflate(R.layout.fragment_selected_files, container, false)
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
