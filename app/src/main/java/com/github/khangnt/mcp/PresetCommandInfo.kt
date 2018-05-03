c3575999418781842f5c57bd35d92676e3aadf22
Simon Pham <simonpham.dn@gmail.com>
2018-05-03 23:15:38 +0700

    Refactoring

--- app/src/main/java/com/github/khangnt/mcp/PresetCommandInfo.kt
+++ app/src/main/java/com/github/khangnt/mcp/PresetCommandInfo.kt
@@ -2,14 +2,9 @@ package com.github.khangnt.mcp
 
 import android.support.annotation.IntRange
 import android.support.annotation.StringRes
-import android.support.v4.app.Fragment
 import com.github.khangnt.mcp.annotation.ConvertType
 import com.github.khangnt.mcp.ui.jobmaker.cmdbuilder.CommandBuilderFragment
 import com.github.khangnt.mcp.ui.jobmaker.cmdbuilder.Mp3CmdBuilderFragment
-import com.github.khangnt.mcp.ui.presetcmd.aac.ConvertAacFragment
-import com.github.khangnt.mcp.ui.presetcmd.flac.ConvertFlacFragment
-import com.github.khangnt.mcp.ui.presetcmd.mp3.ConvertMp3Fragment
-import com.github.khangnt.mcp.ui.presetcmd.mp4.ConvertMp4Fragment
 
 const val TYPE_AUDIO = 0
 const val TYPE_VIDEO = 1
@@ -18,42 +13,7 @@ private fun colorArrayOf(vararg longValues: Long): IntArray {
     return IntArray(longValues.size, { longValues[it].toInt() })
 }
 
-// todo: remove PresetCommand after refactor
-enum class PresetCommand(
-        val type: Int,
-        val shortName: String,
-        val colors: IntArray,
-        @StringRes val titleRes: Int,
-        @StringRes val descriptionRes: Int,
-        val convertFragmentFactory: () -> Fragment
-) {
-    CONVERT_MP3(
-            type = TYPE_AUDIO, shortName = "MP3", colors = colorArrayOf(0xffFC5C7D, 0xff6A82FB), // SublimeLight
-            titleRes = R.string.preset_command_convert_mp3_title,
-            descriptionRes = R.string.preset_command_convert_mp3_des,
-            convertFragmentFactory = ::ConvertMp3Fragment
-    ),
-    CONVERT_AAC(
-            type = TYPE_AUDIO, shortName = "AAC", colors = colorArrayOf(0xff11998e, 0xff38ef7d), // Quepal
-            titleRes = R.string.preset_command_convert_aac_title,
-            descriptionRes = R.string.preset_command_convert_aac_des,
-            convertFragmentFactory = ::ConvertAacFragment
-    ),
-    CONVERT_FLAC(
-            type = TYPE_AUDIO, shortName = "FLAC", colors = colorArrayOf(0xff74ebd5, 0xffACB6E5), // DigitalWater
-            titleRes = R.string.preset_command_convert_flac_title,
-            descriptionRes = R.string.preset_command_convert_flac_des,
-            convertFragmentFactory = ::ConvertFlacFragment
-    ),
-    CONVERT_MP4(
-            type = TYPE_VIDEO, shortName = "MP4", colors = colorArrayOf(0xff2980b9, 0xff2c3e50), // Nighthawk
-            titleRes = R.string.preset_command_convert_mp4_title,
-            descriptionRes = R.string.preset_command_convert_mp4_des,
-            convertFragmentFactory = ::ConvertMp4Fragment
-    )
-    ;
-}
-
+// TODO: create some CmdBuilderFragment
 
 enum class ConvertCommand(
         @ConvertType val type: Int, @StringRes val shortName: Int,
@@ -68,7 +28,26 @@ enum class ConvertCommand(
             colors = colorArrayOf(0xffFC5C7D, 0xff6A82FB), // SublimeLight
             minInputCount = 1, maxInputCount = Int.MAX_VALUE,
             fragmentFactory = ::Mp3CmdBuilderFragment
-    );
+    ),
+    CONVERT_AAC(
+            type = TYPE_AUDIO, shortName = R.string.short_name_aac,
+            colors = colorArrayOf(0xff11998e, 0xff38ef7d), // Quepal
+            minInputCount = 1, maxInputCount = Int.MAX_VALUE,
+            fragmentFactory = ::Mp3CmdBuilderFragment
+    ),
+    CONVERT_FLAC(
+            type = TYPE_AUDIO, shortName = R.string.short_name_flac,
+            colors = colorArrayOf(0xff74ebd5, 0xffACB6E5), // DigitalWater
+            minInputCount = 1, maxInputCount = Int.MAX_VALUE,
+            fragmentFactory = ::Mp3CmdBuilderFragment
+    ),
+    CONVERT_MP4(
+            type = TYPE_VIDEO, shortName = R.string.short_name_mp4,
+            colors = colorArrayOf(0xff2980b9, 0xff2c3e50), // Nighthawk
+            minInputCount = 1, maxInputCount = Int.MAX_VALUE,
+            fragmentFactory = ::Mp3CmdBuilderFragment
+    )
+    ;
 
 }
 
